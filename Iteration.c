7e1f9a6c650a105d84c7bbba6911d0cb24f4b7c3
Mayank Chandel <115147622+Mayank89544@users.noreply.github.com>
2024-12-03 20:41:14 +0530

    Add files via upload

---
+++ Iteration.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+
+double f(double x)
+{
+    return x*x*x*x - x - 10;
+}
+
+double g(double x)
+{
+    return pow((10+x),0.25);
+}
+
+double dg(double x)
+{
+    return 0.25 * (pow((10+x),-0.75));
+}
+
+int main()
+{
+    double x , x0;
+    printf("Enter the calue of x0 : ");
+    scanf("%lf",&x0);
+
+    while(1)
+    {
+        if(fabs(dg(x0)) < 1)
+        {
+            printf("\nCorrect Guess..");
+            break;
+        }
+        else{
+            printf("Wrong Guess!...");
+            scanf("%lf",&x0);
+        }
+    }
+    int iter = 1;
+    x = g(x0);
+    double aerror;
+    printf("\nEnter the allowed error : ");
+    scanf("%lf",&aerror);
+
+    while(fabs(x - x0) >= aerror)
+    {
+        x0 = x;
+        x = g(x0);
+        printf("\nIteration : %d---x0 : %.4lf and x : %.4lf and g(x) : %.4lf",iter , x0 , x , g(x));
+        iter++;
+    }
+    printf("\nThe value is %.4lf in iteration : %d",x,iter);
+}

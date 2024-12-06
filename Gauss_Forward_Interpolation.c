94858fc05cf64790ec3dfbe02d6440276f9aebf0
Mayank Chandel <115147622+Mayank89544@users.noreply.github.com>
2024-12-06 11:37:21 +0530

    Update Gauss_Forward_Interpolation.c

--- Gauss_Forward_Interpolation.c
+++ Gauss_Forward_Interpolation.c
@@ -1,60 +1,67 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-int main()
-{
+#include <stdlib.h>
+
+// Function to calculate factorial
+int fact(int n) {
+    int f = 1;
+    for (int i = 2; i <= n; i++)
+        f *= i;
+    return f;
+}
+
+// Function to calculate u terms for Gauss Forward
+float u_cal(float u, int n) {
+    float temp = u;
+    for (int i = 1; i < n; i++)
+        temp = temp * (u - i);
+    return temp;
+}
+
+int main() {
     int n;
-    printf("Enter n :");
+    printf("Enter the number of data points (n): ");
     scanf("%d", &n);
+
     float x[n], y[n][n];
-    memset(y, 0.0, sizeof(y));
-    printf("Enter the values of x :");
+    
+    // Input the x and y values
+    printf("Enter the values of x:\n");
     for (int i = 0; i < n; i++)
         scanf("%f", &x[i]);
-    printf("Enter the values of y :");
+    
+    printf("Enter the values of y:\n");
     for (int i = 0; i < n; i++)
         scanf("%f", &y[i][0]);
-    printf("Forward Differnece table\n");
-    for (int j = 1; j < n; j++)
-        for (int i = 0; i < n - j; i++)
-            y[i][j] = y[i + 1][j - 1] - y[i][j - 1];
 
-    for (int i = 0; i < n; i++)
-    {
+    // Creating the forward difference table
+    for (int i = 1; i < n; i++) {
         for (int j = 0; j < n - i; j++)
-            printf("%f\t", y[i][j]);
+            y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
+    }
+
+    // Displaying the forward difference table
+    printf("\nForward Difference Table:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i; j++)
+            printf("%0.2f\t", y[i][j]);
         printf("\n");
     }
 
-    printf("Enter the point of interpolation :");
-    float a;
-    scanf("%f", &a);
+    // Interpolation point
+    float value;
+    printf("\nEnter the value of x for interpolation: ");
+    scanf("%f", &value);
 
+    // Calculate u
     float h = x[1] - x[0];
-    float u = (a - x[n / 2]) / h;
+    float u = (value - x[0]) / h;
 
-    float sum = y[n / 2][0], p = 1.0;
-    int i = 0;
-    float data_mul[n], u_mul[n];
-    for (int k = 0; k < n; k++)
-    {
-        data_mul[k] = y[(n / 2) - i][k];
-        if (k % 2)
-            i++;
+    // Apply Gauss Forward Interpolation formula
+    float sum = y[0][0]; // First term
+    for (int i = 1; i < n; i++) {
+        sum += (u_cal(u, i) * y[0][i]) / fact(i);
     }
-    
-    u_mul[0] = p, i = 0;
-    for (int j = 1; j < n; j++)
-    {
-        if (j % 2 == 1)
-            p = p * (u + i) / j, i++;
-        else
-            p = p * (u - i) / j;
-        u_mul[j] = p;
-    }
-    float ans = 0;
-    for (int i = 0; i < n; i++)
-        ans += u_mul[i] * data_mul[i];
-    printf("\n->%f\n\n", ans);
+
+    printf("\nThe interpolated value at x = %.4f is %.4f\n", value, sum);
     return 0;
 }
